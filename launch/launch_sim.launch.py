372a9b4b4937ed85cc30613b28900b731a95ed58
Tanner Festa <festat1@hawkmail.newpaltz.edu>
2025-07-16 07:51:39 -0400

    Cleaned up launch file

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -6,8 +6,16 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, TimerAction
+
 
 from launch_ros.actions import Node
+
+from launch.actions import SetEnvironmentVariable
+
+
+
+
 
 
 
@@ -16,6 +24,8 @@ def generate_launch_description():
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    mute_audio = SetEnvironmentVariable('SDL_AUDIODRIVER', 'dummy')
+
 
     package_name='roomba_bot' #<--- CHANGE ME
 
@@ -27,9 +37,11 @@ def generate_launch_description():
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    PythonLaunchDescriptionSource(os.path.join(
+        get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')),
+        launch_arguments={'gui': 'true'}.items()  # or false for headless
+    )
+
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -37,6 +49,11 @@ def generate_launch_description():
                                    '-entity', 'my_bot'
                                    ],
                         output='screen')
+    spawn_entity_delayed = TimerAction(
+        period=0.0,# seconds
+        actions=[spawn_entity]
+    )
+
     
     # display = IncludeLaunchDescription(
     #     PythonLaunchDescriptionSource([
@@ -56,8 +73,9 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
+        mute_audio,
         rsp,
         gazebo,
-        spawn_entity#,
+        spawn_entity_delayed #spawn_entity
         # display
     ])
